fee31e53b1c4c7d64bf684d3754aff46c95de754
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2023-02-19 10:51:19 -0500

    Add code example without comments.

---
+++ people.py
@@ -0,0 +1,23 @@
+class Person:
+    """
+    Class for a person.
+
+    Attributes:
+        name (str): The name of the person.
+    """
+    def __init__(self, user_provided_name):
+        """
+        Create an instance of `Person`. Set the `Person` instance `name`
+        attribute to the argument provided as `user_provided_name`.
+        """
+        self.name = user_provided_name
+
+    def print_self(self):
+        """
+        Print the `Person` instance. `self` IS the current instance of
+        `Person`.
+        """
+        print(self)
+
+me = Person("MyName")
+me.print_self()
